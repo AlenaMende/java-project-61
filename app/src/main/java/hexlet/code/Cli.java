5626c195ba40de6d888fa831c6497d4f15d3fbb8
AlenaMende <alena.zatokoveenko.9696@gmail.com>
2026-06-02 18:52:15 +0300

    add config

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,11 +2,10 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void run(){
-        Scanner scanner = new Scanner (System.in);
-     System.out.println("May I have your name?");
-     String userName= scanner.next();
-     System.out.println("Hello, " + userName +"!");
-
+    public static void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("May I have your name?");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
     }
 }

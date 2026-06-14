792d91d99533b0106baa8f7a672c1d36917ade0c
AlenaMende <alena.zatokoveenko.9696@gmail.com>
2026-06-14 17:50:28 +0300

    update App file

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,44 @@
 package hexlet.code;
+import java.util.Scanner;
+import hexlet.code.games.EvenGame;
+import hexlet.code.games.CalcGame;
+import hexlet.code.games.GCD;
+import hexlet.code.games.ProgressionGame;
+import hexlet.code.games.PrimeGame;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.run();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter."
+                + "\n 1 - Greet \n 2 - Even \n 3 - Calc \n 4 - GCD \n 5 - Progression \n 6 - Prime \n 0 - Exit");
+        String userChoice = scanner.next();
+        System.out.println("Your choice: " + userChoice);
+
+        switch (userChoice) {
+            case "1":
+                System.out.println("Welcome to the Brain Games!");
+                Cli.run();
+                break;
+            case "2":
+                Engine.run(EvenGame.getRounds(), "Answer 'yes' if the number is even, otherwise answer 'no'.");
+                break;
+            case "3":
+                Engine.run(CalcGame.getRounds(), "What is the result of the expression?");
+                break;
+            case "4":
+                Engine.run(GCD.getRounds(), "Find the greatest common divisor of given numbers.");
+                break;
+            case "5":
+                Engine.run(ProgressionGame.getRounds(), "What number is missing in the progression?");
+                break;
+            case "6":
+                Engine.run(PrimeGame.getRounds(), "Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+                break;
+            case "0":
+                return;
+            default:
+                System.out.println("Unknown choice");
+        }
     }
 }

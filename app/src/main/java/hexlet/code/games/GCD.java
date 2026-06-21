7b5708e01d00b09a2aa9062416a66741616ef1df
AlenaMende <alena.zatokoveenko.9696@gmail.com>
2026-06-21 16:27:38 +0300

    update GCDGame

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -1,8 +1,16 @@
 package hexlet.code.games;
 
+import hexlet.code.Engine;
 import java.util.Random;
 
 public class GCD {
+    public static void play() {
+        String description = "Find the greatest common divisor of given numbers.";
+        String[][] rounds = getRounds();
+
+        Engine.run(rounds, description);
+    }
+
     public static String[][] getRounds() {
         Random random = new Random(); // NOSONAR
 
@@ -13,17 +21,19 @@ public class GCD {
             int b = random.nextInt(99);
 
             String question = a + " " + b;
+            String answer = String.valueOf(getGCD(a, b));
 
-            while (b != 0) {
-                var temp = b;
-                b = a % b;
-                a = temp;
-            }
-
-            String answer = String.valueOf(Math.abs(a));
             rounds[i][0] = question;
             rounds[i][1] = answer;
         }
         return rounds;
+    }
+    public static int getGCD(int a, int b) {
+        while (b != 0) {
+            var temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.abs(a);
     }
 }
